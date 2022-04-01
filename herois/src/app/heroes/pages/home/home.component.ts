9885220811b14215f407cc5fc5bc8d38faec3ee4
goncalveshigino <goncalveshigino20@gmail.com>
2022-04-01 11:15:17 +0100

    hfhfh

--- herois/src/app/heroes/pages/home/home.component.ts
+++ herois/src/app/heroes/pages/home/home.component.ts
@@ -1,22 +1,22 @@
-// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: './home.component.html',
-//   styles: [
-//     `
-//       .container {
-//         margin: 10px
-//       }
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: [
+    `
+      .container {
+        margin: 10px
+      }
 
-//     `
-//   ]
-// })
-// export class HomeComponent implements OnInit {
+    `
+  ]
+})
+export class HomeComponent implements OnInit {
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-// }
+}
